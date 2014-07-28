c96f24340633fcbc1ab6f362f19da103a2ac8863
Otto Urpelainen <oturpe@iki.fi>
2014-07-28 15:32:07 +0100

    Make query interval parameterized

--- butterfly.js
+++ butterfly.js
@@ -3,6 +3,12 @@
 var http = require('http')
 var capParser = require('./cap-parser.js')
 
+// TODO: Load program-wide parameters from configuration file.
+// TODO: Use comment syntax that can be used to auto-generate documentation,
+//       Javadoc style.
+// TODO: Make sure that responses are not cached too much, so that the
+//       butterfly really notices the tornado.
+
 // URL to query for wheather alerts
 var SERVICE_URL = 'http://alerts.weather.gov/cap/us.php?x=0'
 var PARSER_PARAMS = {
@@ -13,9 +19,16 @@ var PARSER_PARAMS = {
     // Severy levels considered urgent
     severe: ['Severe','Extreme']
 }
+// Time between queries for weather information, given in milliseconds.
+// If the previous query has not completed during the interval, the system
+// will wait for another interval, as many times as is needed to get some
+// kind of response.
+//
+// TODO: Make sure that the system does not hang if response is not received
+var QUERY_INTERVAL = 5000
 
 console.log('Starting pulling of warnings from ' + SERVICE_URL)
-setInterval(printWarningCount,5000)
+setInterval(printWarningCount,QUERY_INTERVAL)
 printWarningCount()
 
 var fetching = false
